b9e75715da262f2e6be3baf0ce1a04aaa155d497
twsau <contact.twsau@gmail.com>
2024-12-24 17:26:49 +0000

    tidy up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,29 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
+const MainMenu = () => (
+  <div className="fixed inset-0 place-items-center grid">
+    <Card className="w-full max-w-xs">
+      <CardHeader>
+        <CardTitle className="text-center">Solitaire</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center gap-3">
+        <Link className="w-full" href="play">
+          <Button className="w-full">Play</Button>
+        </Link>
+        <Link className="w-full" href="https://bossweb.dev/projects/solitaire">
+          <Button className="w-full">About</Button>
+        </Link>
+      </CardContent>
+    </Card>
+  </div>
+);
+
 export default function HomePage() {
   return (
     <>
       <Header items={{ right: ["Settings"] }} />
-      <div className="fixed inset-0 place-items-center grid">
-        <Card className="w-full max-w-xs">
-          <CardHeader>
-            <CardTitle className="text-center">Solitaire</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col items-center gap-3">
-            <Link className="w-full" href="play">
-              <Button className="w-full">Play</Button>
-            </Link>
-            <Link
-              className="w-full"
-              href="https://bossweb.dev/projects/solitaire"
-            >
-              <Button className="w-full">About</Button>
-            </Link>
-          </CardContent>
-        </Card>
-      </div>
+      <MainMenu />
     </>
   );
 }
