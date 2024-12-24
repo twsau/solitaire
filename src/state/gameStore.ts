56b04ce4957d30c7c8b78a639825aa8488ddb01f
twsau <contact.twsau@gmail.com>
2024-12-24 17:32:52 +0000

    temporal game state

--- src/state/gameStore.ts
+++ src/state/gameStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { temporal } from "zundo";
 
 interface GameState {
 	grabbed: [];
@@ -21,23 +22,26 @@ interface GameState {
 
 const useGameStore = create<GameState>()(
 	persist(
-		() => ({
-			grabbed: [],
-			grabbedFrom: null,
-			hand: [],
-			waste: [],
-			tableau_1: [],
-			tableau_2: [],
-			tableau_3: [],
-			tableau_4: [],
-			tableau_5: [],
-			tableau_6: [],
-			tableau_7: [],
-			foundation_1: [],
-			foundation_2: [],
-			foundation_3: [],
-			foundation_4: [],
-		}),
+		temporal(
+			() => ({
+				grabbed: [],
+				grabbedFrom: null,
+				hand: [],
+				waste: [],
+				tableau_1: [],
+				tableau_2: [],
+				tableau_3: [],
+				tableau_4: [],
+				tableau_5: [],
+				tableau_6: [],
+				tableau_7: [],
+				foundation_1: [],
+				foundation_2: [],
+				foundation_3: [],
+				foundation_4: [],
+			}),
+			{ limit: 100 }, // maybe set this to something better?
+		),
 		{
 			name: "solitaire-game",
 		},
