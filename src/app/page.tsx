333f8fcbdc95bcd0ace36b77d6056dc436da0e23
twsau <contact.twsau@gmail.com>
2024-12-24 17:22:41 +0000

    main menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import { Header } from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <>
       <Header items={{ right: ["Settings"] }} />
+      <div className="fixed inset-0 place-items-center grid">
+        <Card className="w-full max-w-xs">
+          <CardHeader>
+            <CardTitle className="text-center">Solitaire</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center">
+            <Link href="play">
+              <Button>Play</Button>
+            </Link>
+          </CardContent>
+        </Card>
+      </div>
     </>
   );
 }
--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -3,7 +3,7 @@ import { Header } from "@/components/Header";
 export default function PlayPage() {
   return (
     <>
-      <Header items={{ left: ["HowToPlay"], right: ["Settings"] }} />
+      <Header items={{ right: ["HowToPlay"] }} />
     </>
   );
 }
